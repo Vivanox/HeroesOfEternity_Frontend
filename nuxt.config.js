5311bad13c060df78dd6d59c522f5bc94317b238
Jonas Kervin Hansen <jonas.kerwin.hansen@gmail.com>
2020-05-13 23:00:58 +0200

    Installed vee-validate for frontend validation

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: ['~/plugins/vee-validate.js'],
   /*
    ** Nuxt.js dev-modules
    */
@@ -76,6 +76,8 @@ export default {
    ** Build configuration
    */
   build: {
+    transpile: ['vee-validate/dist/rules'],
+
     /*
      ** You can extend webpack config here
      */
---
+++ plugins/vee-validate.js
@@ -0,0 +1,4 @@
+import { extend } from 'vee-validate'
+import { required, email } from 'vee-validate/dist/rules'
+extend('required', required)
+extend('email', email)
